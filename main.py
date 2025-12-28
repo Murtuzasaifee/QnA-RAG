69af74aff8a5c837340c90d87e3e42cdb168624b
Murtuza Saifee <msaifee@firstorion.com>
2025-12-28 08:30:03 +0400

    Document processing added

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def main():
+from app.core.document_processor import DocumentProcessor
+import hydra
+from omegaconf import DictConfig
+from app.utils.config_utils import print_config, validate_config, with_config
+
+@with_config
+def main(cfg: DictConfig):
     print("Hello from qna-rag!")
+    document_processor = DocumentProcessor(cfg)
+    document_processor.process_file("test.pdf")
 
 
 if __name__ == "__main__":
